a0f211e416e72df78324dd904e15a395d76eb682
Siripongapr <siripong.api43@gmail.com>
2024-07-24 21:23:37 +0700

    feat: Initialize the firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:np_app_test/firebase_options.dart';
 import 'package:np_app_test/router.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
